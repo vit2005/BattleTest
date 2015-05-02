a666a0b506a03b52099a4da61f48379b95b90243
Vit <elstan88@mail.ru>
2015-05-03 01:50:56 +0300

    Unity

---
+++ SingleBattleTest/Skills/SPoison.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingleBattleTest.Effect;
+
+namespace SingleBattleTest.Skills
+{
+    public class SPoison : ISkill
+    {
+        public SPoison(byte lvl)
+        {
+            base.id = "3";
+            base.name = "Strike";
+            base.description = "Hit enemy";
+            base.type = skillType.attack;
+            base.onEnemy = true;
+            base.lvl = lvl;
+        }
+
+        public override void Use(ref List<Unit> allys, ref List<Unit> enemies, ref Unit u1, ref Unit u2)
+        {
+            System.Threading.Thread.Sleep(3);
+            Random r = new Random();
+            if (r.Next(100) < Math.Sqrt(lvl * 25) + 55) //TODO poison resist
+                u2.Effects.Add(new EPoison());
+        }
+    }
+}
---
+++ UnitySolution/TheGame/Assets/Scripts/CenterlizeScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterlizeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.position = new Vector3((float)598, (float)(336.5), 0);
+		//gameObject.transform.position = new Vector3(0, 0, 0);
+	}
+}
